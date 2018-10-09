1d85720618c02ac8245cf49215c8b957f8e9bd60
Curt Liu <System3748@outlook.com>
2018-10-09 17:47:06 +0800

    Add files via upload

---
+++ CrossOrigin.php
@@ -0,0 +1,8 @@
+<?php
+        /*header('Access-Control-Allow-Origin:*');
+        header('Content-type: image/png');
+        if (isset($_GET['url'])) {
+                echo file_get_contents($_GET['url']);
+        }*/
+        phpinfo();
+?>
